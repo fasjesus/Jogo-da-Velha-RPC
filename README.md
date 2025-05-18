3f6168af59579d6f81ef44b74a8909a98e77c7aa
fasjesus <developerflavia@gmail.com>
2025-05-18 18:32:17 -0300

    feat: add option 3 parts

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Jogo da Velha 5x5
 
 <p align="justify">
-    <strong>Jogo da Velha 5x5</strong> é um jogo de tabuleiro no estilo "cinco em linha" jogado em uma grade 5x5, onde três jogadores competem para alinhar cinco peças consecutivas em linha reta, seja horizontal, vertical ou diagonal.<br><br>
+    <strong>Jogo da Velha 5x5</strong> é um jogo de tabuleiro no estilo "cinco em linha" jogado em uma grade 5x5, onde três jogadores competem para alinhar cinco peças consecutivas (ou 3, na pasta "Três Peças") em linha reta, seja horizontal, vertical ou diagonal.<br><br>
     Este projeto implementa uma versão distribuída do jogo utilizando chamadas remotas de procedimento (RPC), permitindo que os jogadores se conectem e interajam em diferentes salas de jogo.
 </p>
 
@@ -31,8 +31,8 @@ python client.py
 ```
 ### Regras
 <p align="justify">
-    - O objetivo é alinhar cinco peças consecutivas em uma linha reta (horizontal, vertical ou diagonal).<br><br>
-    - Três jogadores se alternam para colocar suas peças na grade 5x5 até que um deles consiga alinhar cinco peças seguidas.
+    - O objetivo é alinhar cinco (ou 3) peças consecutivas em uma linha reta (horizontal, vertical ou diagonal).<br><br>
+    - Três jogadores se alternam para colocar suas peças na grade 5x5 até que um deles consiga alinhar cinco (ou 3) peças seguidas.
     <br><br>
     - Para fazer a jogada digite no seguinte formato:
 </p>
---
+++ Três Peças/client.py
@@ -0,0 +1,54 @@
+# Flávia Alessandra Santos de Jesus.
+
+import xmlrpc.client
+import time
+
+servidor = xmlrpc.client.ServerProxy("http://localhost:8080/")
+
+nome = input("Digite o seu nome para se registrar: ")
+
+resposta_registro = servidor.registrar_jogador(nome)
+print(resposta_registro)
+
+def imprimir_tabuleiro(tabuleiro):
+    tamanho = len(tabuleiro)
+    linha_delimitadora = "+" + "---+" * tamanho
+    j = 1
+    print("  " + "   ".join(str(i) for i in range(1, tamanho + 1)))
+    for linha in tabuleiro:
+        print(linha_delimitadora)
+        linha_formatada = "| " + " | ".join(celula if celula != " " else " " for celula in linha) + f" | {j}"
+        print(linha_formatada)
+        j += 1
+    print(linha_delimitadora)
+
+if "registrado com sucesso" in resposta_registro:
+    jogo_em_andamento = True
+    while jogo_em_andamento:
+        tabuleiro, jogador_atual_servidor, jogo_em_andamento_servidor = servidor.obter_tabuleiro()
+
+        print("\nTabuleiro atual:")
+        imprimir_tabuleiro(tabuleiro)
+
+        if not jogo_em_andamento_servidor:
+            print("O jogo terminou!")
+            break
+
+        jogadores = servidor.obter_jogadores()
+
+        if nome == jogadores[jogador_atual_servidor]:
+            print("É sua vez de jogar!")
+            posicao = input("Digite a posição [Linha Coluna]: ")
+            try:
+                linha, coluna = map(int, posicao.split())
+                resultado = servidor.movimento(nome, linha - 1, coluna - 1)
+            except Exception:
+                resultado = "Entrada inválida! Use formato: linha coluna (ex: 1 3)"
+            print(resultado)
+
+            if "venceu" in resultado or "empate" in resultado:
+                jogo_em_andamento = False
+        else:
+            print("Aguardando o adversário...")
+
+        time.sleep(1)
